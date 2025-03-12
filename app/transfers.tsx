b1f51dacf43b2352dd20773c4b3dc0214d4e8775
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-12 20:36:47 +0100

    Transfert 2

---
+++ app/transfers.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TransfersScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Transfers Screen</Text>
+    </View>
+  );
+}
