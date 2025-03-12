d67572b1a43ee55ab6ac55846e665783cbdfded2
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-12 20:40:00 +0100

    Help

---
+++ app/help.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HelpScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Help & Support</Text>
+    </View>
+  );
+}
