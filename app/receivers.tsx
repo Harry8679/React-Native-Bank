a3e961888b2e30585b46505616d67b468c4a97f5
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-12 20:39:05 +0100

    receiver 2

---
+++ app/receivers.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ReceiversScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Receivers List</Text>
+    </View>
+  );
+}
