010c8f728c28e5f289522b572dee817bd900552b
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-14 06:03:06 +0100

    Paiement screen

---
+++ app/sodec/paiement.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function PaiementScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "white", justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Page Paiement</Text>
+    </View>
+  );
+}
