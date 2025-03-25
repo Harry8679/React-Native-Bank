3665f02e9dce83c8816ab22999140279ca527591
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-25 18:35:51 +0100

    Confirmation Screen

--- app/agency/agency-confirmation.tsx
+++ app/agency/agency-confirmation.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import { useRouter, useLocalSearchParams } from "expo-router";
 import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function ConfirmationScreen() {
   const { client, amount } = useLocalSearchParams();
@@ -9,6 +10,18 @@ export default function ConfirmationScreen() {
 
   return (
     <View style={{ flex: 1, backgroundColor: "white", padding: 20 }}>
+      
+      {/* 🔙 Flèche de retour */}
+      <TouchableOpacity 
+        onPress={() => {
+          if (router.canGoBack()) router.back();
+          else router.push("/sodec");
+        }}
+        style={{ position: "absolute", top: 50, left: 20, zIndex: 10 }}
+      >
+        <Ionicons name="arrow-back" size={40} color="black" />
+      </TouchableOpacity>
+
       <Image source={require("../../assets/gimacpay.png")} style={{ width: 200, height: 70, alignSelf: "center" }} />
       <Text style={{ fontSize: 24, textAlign: "center", marginVertical: 20 }}>ENVOI DE DÉPÔT</Text>
 
@@ -26,9 +39,7 @@ export default function ConfirmationScreen() {
         <TouchableOpacity
           onPress={() => {
             pin === "123456"
-              // ? router.push("/sodec/agency/agency-success")
               ? router.push("/agency/agency-success")
-              // : router.push("/sodec/agency/agency-error");
               : router.push("/agency/agency-error");
           }}
           style={styles.btn}
