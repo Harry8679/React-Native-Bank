9c4b9084b7888edd0f3a83fcde3a8cf536868f95
Harry MacCode <emarh.harry.code@gmail.com>
2025-04-03 23:49:50 +0200

    Order of countries

--- app/send/international.tsx
+++ app/send/international.tsx
@@ -4,7 +4,7 @@ import { View, Text, TextInput, TouchableOpacity, Modal, FlatList } from "react-
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
-const countries = ["🇫🇷 France", "🇺🇸 USA", "🇩🇪 Allemagne", "🇪🇸 Espagne", "🇨🇮 Cote d'Ivoire", "🇳🇬 Nigeria", "🇨🇳 Chine", "🇯🇵 Japon", "🇦🇺 Australie"];
+const countries = ["🇩🇪 Allemagne", "🇦🇺 Australie", "🇨🇳 Chine", "🇨🇮 Cote d'Ivoire", "🇪🇸 Espagne", "🇫🇷 France", "🇯🇵 Japon", "🇳🇬 Nigeria", "🇺🇸 USA"];
 const terminaisons = ["MTN", "Orange", "Visa"];
 const institutions = ["Western Union", "MoneyGram", "RIA"];
 
